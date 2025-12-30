011923fbad3d3bec52c2dc5a3265d28cd4c3148b
Vladimir Shevelevich <vshevelevich@playmous.com>
2025-12-30 10:57:35 +0100

    add main scene and scope

---
+++ Assets/App/Scripts/ProjectScope.cs
@@ -0,0 +1,13 @@
+﻿using VContainer;
+using VContainer.Unity;
+
+namespace App
+{
+    public class ProjectScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+               
+        }
+    }
+}
