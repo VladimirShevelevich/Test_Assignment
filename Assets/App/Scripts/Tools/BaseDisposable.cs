24742364d52470100d1345e8ea53f40575b19d34
Vladimir Shevelevich <vshevelevich@playmous.com>
2025-12-31 11:52:57 +0100

    add tools assdef and basedisposable

---
+++ Assets/App/Scripts/Tools/BaseDisposable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UniRx;
+
+namespace Arkanoid.Tools.Disposable
+{
+    public abstract class BaseDisposable : IDisposable
+    {
+        private CompositeDisposable _compositeDisposable;
+
+        protected void AddDisposable(IDisposable disposable)
+        {
+            _compositeDisposable ??= new CompositeDisposable();
+            _compositeDisposable.Add(disposable);
+        }
+
+        public virtual void Dispose()
+        {
+            _compositeDisposable.Dispose();
+        }
+    }
+}
