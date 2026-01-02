26e8d0fa40cdd49c71943b574b02fbbb46950923
Vladimir Shevelevich <vshevelevich@playmous.com>
2026-01-02 11:53:37 +0100

    add counter

---
+++ Assets/App/Scripts/Cards/Decks/DeckCounterView.cs
@@ -0,0 +1,29 @@
+﻿using App.Cards.Deck;
+using TMPro;
+using UniRx;
+using UnityEngine;
+
+namespace App.Scripts.Cards.Decks
+{
+    public class DeckCounterView : MonoBehaviour
+    {
+        [SerializeField] private DeckView _deckView;
+        [SerializeField] private TMP_Text _counterText;
+
+        private void Start()
+        {
+            _deckView.CardsAmount.Subscribe(OnCardAmountChanged).
+                AddTo(gameObject);
+        }
+
+        private void OnCardAmountChanged(int amount)
+        {
+            UpdateCounter(amount);
+        }
+
+        private void UpdateCounter(int amount)
+        {
+            _counterText.text = amount.ToString();
+        }
+    }
+}
