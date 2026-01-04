16e54c11321af4c923021e13b85306052f4e98f0
Vladimir Shevelevich <vshevelevich@playmous.com>
2026-01-04 12:13:42 +0100

    handle initialization cancelation

--- Assets/App/Scripts/MagicWords/Data/WordsDataLoader.cs
+++ Assets/App/Scripts/MagicWords/Data/WordsDataLoader.cs
@@ -7,15 +7,12 @@ using UnityEngine;
 
 namespace App.MagicWords
 {
-    public class WordsDataLoader : BaseDisposable
+    public class WordsDataLoader
     {
         public WordsData Data { get; private set; }
         
         private readonly MagicWordsContent _magicWordsContent;
         private readonly MessageService _messageService;
-
-        //Object dispose handling token
-        private readonly CancellationTokenSource _cts = new();
 
         public WordsDataLoader(MagicWordsContent magicWordsContent, MessageService messageService)
         {
@@ -23,45 +20,43 @@ namespace App.MagicWords
             _messageService = messageService;
         }
         
-        public async UniTask InitializeAsync()
+        public async UniTask InitializeAsync(CancellationToken ctsToken)
         {
-            AddDisposable(new TokenDisposer(_cts));
-            
             while (true)
             {
                 try
                 {
-                    await LoadData();
+                    await LoadData(ctsToken);
                     Debug.Log("Words data has been loaded");
                     return;
                 }
                 catch (OperationCanceledException)
                 {
                     Debug.Log($"Data loading has been canceled.");
-                    return;
+                    throw;
                 }        
                 catch (Exception e)
                 {
                     Debug.LogWarning($"Data loading has failed. {e}");
-                    await WaitUntilRepeatIsCalled();
+                    await WaitUntilRepeatIsCalled(ctsToken);
                 }        
             }    
         }
 
-        private async UniTask LoadData()
+        private async UniTask LoadData(CancellationToken token)
         {
             Debug.Log("Dialogue data loading");
-            await UniTask.Delay(2000, cancellationToken: _cts.Token);
-            var json = await DataLoader.LoadJsonAsync(_magicWordsContent.DataUrl, _cts.Token);
+            await UniTask.Delay(2000, cancellationToken: token);
+            var json = await DataLoader.LoadJsonAsync(_magicWordsContent.DataUrl, token);
             Data = DataParser.Parse<WordsData>(json);
         }
 
-        private async UniTask WaitUntilRepeatIsCalled()
+        private async UniTask WaitUntilRepeatIsCalled(CancellationToken token)
         {
             var repeatIsCalled = false;
             ShowLoadingFailedMessage(onRepeatCalled: () => repeatIsCalled = true);
             await UniTask.WaitUntil(() => repeatIsCalled, 
-                cancellationToken: _cts.Token);
+                cancellationToken: token);
         }
 
         private void ShowLoadingFailedMessage(Action onRepeatCalled)
--- Assets/App/Scripts/MagicWords/InitializationQueue.cs
+++ Assets/App/Scripts/MagicWords/InitializationQueue.cs
@@ -1,13 +1,21 @@
-﻿using Cysharp.Threading.Tasks;
+﻿using System;
+using System.Threading;
+using App.Scripts.Tools;
+using App.Tools;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
 using VContainer.Unity;
 
 namespace App.MagicWords
 {
-    public class InitializationQueue : IInitializable
+    public class InitializationQueue : BaseDisposable, IInitializable
     {
         private readonly WordsDataLoader _wordsDataLoader;
         private readonly DialogueDataLoader _dialogueDataLoader;
 
+        //Object dispose handling token
+        private readonly CancellationTokenSource _cts = new();
+        
         public InitializationQueue(WordsDataLoader wordsDataLoader, DialogueDataLoader dialogueDataLoader)
         {
             _wordsDataLoader = wordsDataLoader;
@@ -16,13 +24,21 @@ namespace App.MagicWords
         
         public void Initialize()
         {
+            AddDisposable(new TokenDisposer(_cts));
             InitializeAsync().Forget();
         }
 
         private async UniTaskVoid InitializeAsync()
         {
-            await _wordsDataLoader.InitializeAsync();
-            _dialogueDataLoader.Initialize();
+            try
+            {
+                await _wordsDataLoader.InitializeAsync(_cts.Token);
+                _dialogueDataLoader.Initialize();
+            }
+            catch (OperationCanceledException)
+            {
+                Debug.Log("Initialization has been canceled");
+            }
         }
     }
 }
