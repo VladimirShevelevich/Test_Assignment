a9b09115a84b3d03b692ccbbaebd89a43e5abaaf
Vladimir Shevelevich <vshevelevich@playmous.com>
2026-01-06 12:27:04 +0100

    implement emoji map

--- Assets/App/Scripts/MagicWords/Content/EmojisMap.cs
+++ Assets/App/Scripts/MagicWords/Content/EmojisMap.cs
@@ -1,29 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace App.MagicWords
 {
-    
-
     [CreateAssetMenu(fileName = "EmojisMap", menuName = "Content/EmojisMap")]
     public class EmojisMap : ScriptableObject
     {
-        [Serializable]
-        public class EmojiData
+       
+        private readonly Dictionary<string, string> _datas = new()
         {
-            public string Key;
-            public string Emoji;
-        }
-
-        [SerializeField] private List<EmojiData> _datas;
+            {"satisfied", "\ud83d\ude0c"},
+            {"intrigued", "\ud83e\udd14"},
+            {"neutral", "\ud83d\ude10"},
+            {"affirmative", "\ud83d\udc4d"},
+            {"laughing", "\ud83d\ude06"},
+            {"win", "\ud83c\udfc6"},
+        };
 
         public string GetEmojiByKey(string key)
         {
-            var data = _datas.FirstOrDefault(x => x.Key == key);
-            if (data != null) 
-                return data.Emoji;
+            if (_datas.TryGetValue(key, out var emoji))
+            {
+                return emoji;
+            }
             
             Debug.LogError($"The emojis map doesn't contain [{key}] emoji data");
             return null;
